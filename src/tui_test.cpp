e8600c2e74b139553c1478477c7b9378ead2b94d
AlanDorantes <160064878+AlanDorantes@users.noreply.github.com>
2024-05-11 00:32:17 +0000

    Version 5

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -6,17 +6,16 @@
 #include <thread>
 #include <experimental/random>
 #include <list>
-#include<fstream>
+#include <fstream>
 
 using namespace std;
 using namespace ftxui;
-
 
 int main(int argc, char const *argv[])
 {
     list<string> textos;
     fstream imagen;
-    imagen.open("./assets/imagen.txt");
+    imagen.open("./assets/Derecha.txt");
 
     string linea;
     while (getline(imagen, linea))
@@ -25,49 +24,75 @@ int main(int argc, char const *argv[])
     }
     imagen.close();
 
-    int fotograma =0;
+
+    int fotograma = 0;
 
     string reset;
-    int posX = 0;
+    int posX = 1;
     int posY = 6;
     while (true)
     {
-        fotograma++;
-
-        int r = std::experimental::randint(0, 255);
-        int g = std::experimental::randint(0, 255);
-        int b = std::experimental::randint(0, 255);
-
-        Element personaje = spinner(21,fotograma);
-        Decorator colorFondo = bgcolor(Color::RGB(r, g, b));
-        Decorator colorTexto = color(Color::RGB(b, r, g));
-        Element dibujo = border({
-            hbox(personaje) 
-        }) | colorFondo | colorTexto;
-
-        Dimensions Alto = Dimension::Full();
-        Dimensions Ancho = Dimension::Full();
-
-        Screen pantalla = Screen::Create(Ancho,Alto);
-        Render(pantalla,dibujo);
         
-        int l = 0;
-        for (auto &&texto : textos)
-        {
-            int i = 0;
-            for (auto &&letra : texto)
-            {
-                pantalla.PixelAt(posX + i, posY + l).character = letra;
-                i ++;
-            }
-            l++;
-        }
-        posX++;
+        Decorator colorFondo = bgcolor(Color::GrayDark);
+        Decorator colorTexto = color(Color::GrayLight);
+        Element dibujo = border({hbox()}) | colorFondo | colorTexto;
 
+        Dimensions Alto = Dimension::Fixed(25);
+        Dimensions Ancho = Dimension::Fixed(70);
+
+        Screen pantalla = Screen::Create(Ancho, Alto);
+        Render(pantalla, dibujo);
+
+        if (posX <= 20)
+        {
+            for (auto &&texto : textos)
+            {
+                int i = 0;
+                for (auto &&letra : texto)
+                {
+                    pantalla.PixelAt(posX + i, 8).character = letra;
+                    i++;
+                }
+            }
+            posX++;
+        }
+
+        if (posX > 20)
+        {
+            if(posY <= 12)
+        {
+            for (auto &&texto : textos)
+            {
+                int l = -4;
+                for (auto &&letra : texto)
+                {
+                    pantalla.PixelAt(26, posY + l).character = letra;
+                    l++;
+                }
+            }
+        posY++;
+        }
+        }
+
+        if (posY > 11)
+        {
+            for (auto &&texto : textos)
+            {
+                int i = -1;
+                for (auto &&letra : texto)
+                {
+                    pantalla.PixelAt(posX + i, 13).character = letra;
+                    i++;
+                }
+            }
+            posX++;
+        }
+
+       
         pantalla.Print();
         reset = pantalla.ResetPosition();
         cout << reset;
-        this_thread::sleep_for(0.05s);
+        this_thread::sleep_for(0.75s);
     }
 
     return 0;
